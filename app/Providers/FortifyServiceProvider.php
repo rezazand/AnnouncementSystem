a63d07ac4c9dc3e140b087290c2baba521a2ddf5
rezazand <rezazand@users.noreply.github.com>
2022-06-15 11:24:30 +0430

    some config for fortify.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,8 +6,10 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
@@ -36,14 +38,33 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
+        Fortify::authenticateUsing(function (Request $request){
+            $user = User::where('email', $request->email)
+                ->orWhere('PNumber', $request->email)
+                ->first();
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                return $user;
+
+            }
+        });
+
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
-
             return Limit::perMinute(5)->by($email.$request->ip());
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        Fortify::loginView(function (){
+            return view('auth.login');
+        });
+
+        Fortify::requestPasswordResetLinkView(function (){
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(function (Request $request){
+            return view('auth.reset-password',['request'=>$request]);
         });
     }
 }
+    
